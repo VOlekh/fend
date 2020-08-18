b25b09f1b23cf345717d30e2a708be707c68118d
Valentina-Olekhnovich <volekh@gmail.com>
2020-08-18 23:21:26 +0200

    Unit Testing using Jest Framework

--- projects/evaluate-news-nlp/__test__/testFormHandler.spec.js
+++ projects/evaluate-news-nlp/__test__/testFormHandler.spec.js
@@ -1,24 +1,5 @@
 //Import the js file to test
 import { handleSubmit } from '../src/client/js/formHandler'
-//Define the input for the function.
-//Define the expected output
-//Check if the function produces the expected output
-// The describe() function takes two arguments - a string description, 
-//and a test suite as a callback function.  
-// A test suite may contain one or more related tests    
-
     test("Testing if the handleSubmit() function exists", () => {
-           // Define the input for the function, if any, in the form of variables/array
-           // Define the expected output, if any, in the form of variables/array
-           // The expect() function, in combination with a Jest matcher, is used to check if the function produces the expected output
-           // The general syntax is `expect(myFunction(arg1, arg2, ...)).toEqual(expectedValue);`, where `toEqual()` is a matcher
-           expect(handleSubmit.toBeDefined();
-});
-
-const request = require('supertest');
-const app = require('../server/index');
-
-test('Respond with 200 status code', async () => {
-const response = await request(app).get('/');
-expect(response.statusCode).toBe(200);
+           expect(handleSubmit).toBeDefined();
 });
--- projects/evaluate-news-nlp/__test__/testNameChecker.spec.js
+++ projects/evaluate-news-nlp/__test__/testNameChecker.spec.js
@@ -1,13 +1,11 @@
 //Import the js file to test
 import { checkForName } from '../src/client/js/nameChecker'
 
-test('testing sidet data', () => {
-    expect(checkForName('/test')).resolves.toBe({
+test('testing nameChecker function', () => {
 //Define the input for the function.
-        'title':'kbkb',
+const input = 'Picard';
 //Define the expected output
-        'message':'this is a message',
-        'time': 'now'
-      })
-})
-//Check if the function produces the expected output????
+const output = true;
+//Check if the function produces the expected output
+expect(checkForName(input, true)).toEqual(output);     
+ });
